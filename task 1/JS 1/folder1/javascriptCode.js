e7f2251550abd3158c04c793fa4d4515ce00fe86
Chordify <edwin.antony@chordify.com>
2023-09-28 13:59:13 +0530

    task1

---
+++ task 1/JS 1/folder1/javascriptCode.js
@@ -0,0 +1,16 @@
+let d="my name is"
+let a = "edwin"
+let b = "antony"
+let c = a + b;
+document.write(d.concat("  ",a,"  ",b)) 
+document.write("<br>");
+document.write("my pattern is :")
+document.write("<br>");
+let string = "";
+for (i = 0; i < 5; i++) {
+    for (j = 0; j <= 5; j++) {
+        string += '*'
+    }
+    string += "<br>"
+}
+document.write(string)
